f4c0af0868a44b98df3bee701307b60b7eaeb575
KiranPK12 <kiranpadma2003@gmail.com>
2023-01-29 13:36:25 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,12 +5,10 @@ import About from './pages/About'
 import SingleCocktail from './pages/SingleCocktail'
 import Error from './pages/Error'
 import Navbar from './components/Navbar'
-import Empty from './components/empty'
 function App() {
   return (
     <Router>
       <Navbar/>
-      <Empty/>
        <Routes>
           <Route path='/' element={<Home/>}> </Route>
           <Route path='/about' element={<About/>}> </Route>
